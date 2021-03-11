2a1e423ec5cb47cf36eb2f171ba3db4e62a980e0
cavilosa <cavilosa33@gmail.com>
2021-03-11 09:31:45 -0700

    updated table with spam content to innocuous string

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -2,6 +2,7 @@
 
 import datetime
 import psycopg2
+import bleach
 
 DBNAME = 'forum'
 
@@ -9,6 +10,7 @@ def get_posts():
     """Return all posts from the 'database', most recent first."""
     conn = psycopg2.connect(database=DBNAME)
     cursor = conn.cursor()
+    cursor.execute("update posts set content = 'cheese' where content like '%spam%'")
     cursor.execute('select content, time from posts order by time desc')
     return cursor.fetchall()
     conn.close()
@@ -17,6 +19,6 @@ def add_post(content):
     """Add a post to the 'database' with the current timestamp."""
     conn = psycopg2.connect(database=DBNAME)
     cursor = conn.cursor()
-    cursor.execute("insert into posts values (%s)", (content,))
+    cursor.execute("insert into posts values (%s)", (bleach.clean(content),))
     conn.commit()
     conn.close()
